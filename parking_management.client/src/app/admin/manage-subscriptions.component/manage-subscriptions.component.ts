e065c82f8ed7e57ba6584cf8f38b70f9a96f5fd6
Manoj Kumar GA <Manoj@vrize.com>
2026-08-30 21:16:32 +0530

    added admin components

---
+++ parking_management.client/src/app/admin/manage-subscriptions.component/manage-subscriptions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-subscriptions.component',
+  standalone: false,
+  templateUrl: './manage-subscriptions.component.html',
+  styleUrl: './manage-subscriptions.component.css',
+})
+export class ManageSubscriptionsComponent {
+
+}
